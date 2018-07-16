74c9b44cfd2aff3a5111e0aa4e482ec66d9da8c2
hbowden <2trill2spill@gmail.com>
2018-07-16 14:17:08 -0500

    Start switching to webpack 4.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,6 @@
-let path = require("path");
-let webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack"); // eslint-disable-line
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 let config = {
   "entry": {
@@ -48,6 +48,18 @@ let config = {
     "filename": "[name]/[name].js",
     "libraryTarget": "umd"
   },
+  "optimization": {
+    "splitChunks": {
+      "cacheGroups": {
+        "styles": {
+          "name": "styles",
+          "test": /\.css$/,
+          "chunks": "all",
+          "enforce": true
+        }
+      }
+    }
+  },
   "externals": [
     {
       "react": {
@@ -69,65 +81,35 @@ let config = {
   "module": {
     "rules": [
       {
-        "test": /.js?$/,
-        "loader": "babel-loader",
-        "exclude": /node_modules/
+        "test": /\.js$/,
+        "exclude": /node_modules/,
+        "use": {
+          "loader": "babel-loader"
+        }
+      },
+      {
+        "test": /\.css$/,
+        "use": [
+          MiniCssExtractPlugin.loader,
+          {
+            "loader": "css-loader",
+            "query": {
+              "modules": true,
+              "importLoaders": 1,
+              "localIdentName": "ra_[name]__[local]",
+              "minimize": true
+            }
+          },
+          "postcss-loader"
+        ]
       }
     ]
   },
   "plugins": [
-    new webpack.DefinePlugin({
-      "process.env": {
-        "NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-      }
+    new MiniCssExtractPlugin({
+      "chunkFilename": "atlasThemes.min.css"
     })
   ]
 };
-
-
-if (process.env.NODE_ENV === "production") {
-  config.module.rules.push({
-    "test": /\.css$/,
-    "loader": ExtractTextPlugin.extract({
-      "fallback": "style-loader",
-      "use": [
-        {
-          "loader": "css-loader",
-          "query": {
-            "modules": true,
-            "importLoaders": 1,
-            "localIdentName": "ra_[name]__[local]",
-            "minimize": false
-          }
-        },
-        {
-          "loader": "postcss-loader",
-          "options": {
-            "config": {
-              "path": "config/postcss.config.js"
-            }
-          }
-        }
-      ]
-    })
-  });
-  config.plugins.push(new ExtractTextPlugin("dontImport.min.css"));
-} else {
-  config.module.rules.push({
-    "test": /\.css$/,
-    "loaders": [
-      "style-loader?sourceMap",
-      "css-loader?modules&importLoaders=1&localIdentName=ra_[name]__[local]",
-      {
-        "loader": "postcss-loader",
-        "options": {
-          "config": {
-            "path": "config/postcss.config.js"
-          }
-        }
-      }
-    ]
-  });
-}
 
 module.exports = config;
